54d32df12b6a03e5b35f5a75573cde0a5033c891
Stardust_Asher <149375124+StardustAsher@users.noreply.github.com>
2025-02-17 16:26:01 +0100

    creada clase

---
+++ TheatrumMundiProyect/TheatrumMundi/WriteTextComponent.cpp
@@ -0,0 +1,17 @@
+#include "WriteTextComponent.h"
+
+WriteTextComponent::WriteTextComponent()
+{
+}
+
+void WriteTextComponent::update()
+{
+}
+
+void WriteTextComponent::render()
+{
+}
+
+WriteTextComponent::~WriteTextComponent()
+{
+}
