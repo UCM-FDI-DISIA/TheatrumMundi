40c23cb23aa89576dabbe20241c5f964508afc30
Andrea G.A. <149385951+andreaGonzAnas@users.noreply.github.com>
2025-02-17 11:12:01 +0100

    Boton en escena muestra log
    Hay que refactorizar el log

--- TheatrumMundiProyect/TheatrumMundi/DebugLogRoom.cpp
+++ TheatrumMundiProyect/TheatrumMundi/DebugLogRoom.cpp
@@ -6,6 +6,12 @@
 
 #include "Log.h"
 
+#include "../src/components/ClickComponent.h"
+#include "../src/components/TriggerComponent.h"
+#include "../src/components/DragComponent.h"
+#include "../src/components/CircleArea2D.h"
+#include "../src/components/RectArea2D.h"
+
 DebugLogRoom::DebugLogRoom() : SceneRoomTemplate()
 {
 	_sceneLog = new Log();
@@ -22,6 +28,23 @@ void DebugLogRoom::init()
 		auto _fighterTransform = entityManager->addComponent<Transform>(_fighter, Vector2D(0, 0), Vector2D(0, 0), 500, 500, 0);
 		entityManager->addComponent<Image>(_fighter, &sdlutils().images().at("prueba"));
 
+		entityManager->addComponent<RectArea2D>(_fighter);
+
+		ClickComponent* clk = entityManager->addComponent<ClickComponent>(_fighter);
+		clk->connect(ClickComponent::JUST_CLICKED, [this]()
+			{ 
+				std::cout << "CLICKED\n"; 
+				_sceneLog->showLog();
+			});
+
+		TriggerComponent* trg = entityManager->addComponent<TriggerComponent>(_fighter);
+		trg->connect(TriggerComponent::JUST_ENTERED, []() { std::cout << "ENTERED\n";  });
+		trg->connect(TriggerComponent::JUST_LEFT, []() { std::cout << "LEFT\n";  });
+
+		DragComponent* drg = entityManager->addComponent<DragComponent>(_fighter);
+		drg->connect(DragComponent::DRAG, []() { std::cout << "DRAGGING\n"; });
+
+
 		//add debug lines to log
 		_sceneLog->addDialogueLineLog("ev1", { "authorX", "hola que tal" });
 		_sceneLog->addDialogueLineLog("ev1", { "authorY", "aaaa" });
@@ -35,7 +58,7 @@ void DebugLogRoom::init()
 		_sceneLog->addDialogueLineLog("ev1", { "authorY", "aaaa" });
 		_sceneLog->addDialogueLineLog("ev1", { "authorY", "/" });
 
-		_sceneLog->showLog();
+		
 	}
 }
 
