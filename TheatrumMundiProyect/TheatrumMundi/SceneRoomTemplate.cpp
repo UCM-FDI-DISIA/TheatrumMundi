61535d91d5b2883aa0a01b0c26414985d72697ce
davidfb1305 <149385933+davidfb1305@users.noreply.github.com>
2025-02-18 20:14:59 +0100

    Paso algo raro con la escena inicial ya esta arreglado

--- TheatrumMundiProyect/TheatrumMundi/SceneRoomTemplate.cpp
+++ TheatrumMundiProyect/TheatrumMundi/SceneRoomTemplate.cpp
@@ -4,7 +4,7 @@
 
 SceneRoomTemplate::SceneRoomTemplate(): SceneTemplate()
 {
-	//for (int i = 0; i < 3;i++)puzzlesol.push_back(false);
+	for (int i = 0; i < 3;i++)puzzlesol.push_back(false);
 	//init inventory
 }
 
