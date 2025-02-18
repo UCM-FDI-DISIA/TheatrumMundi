30c6263f0b060fb39a6a9bbb1a92f592948fd8c0
Reycka <157039413+Reycka@users.noreply.github.com>
2025-02-18 12:41:46 +0100

    ReadDialogue
    - Se ha creado un tipo enumerado para el paso de eventos
    - Se ha creado un parseador de eventos a string para leerlo en el map
    - Se ha creado un readAnswer que a su vez actualiza el valor de la Sala al Manager
    - Si gusta en probarlo solo debe de llamar al método en el Game para que se lea
    
    IMPORTANTE: Voy a probar a hacerlo SingleTon para poder llamarlo desde donde sea, ya sea en las salas de puzzles, en el log...

--- TheatrumMundiProyect/TheatrumMundi/DialogueManager.h
+++ TheatrumMundiProyect/TheatrumMundi/DialogueManager.h
@@ -8,6 +8,17 @@ struct TextInfo {
 	std::string Character;
 	std::string Text;
 };
+enum eventToRead {
+	SalaIntermediaEvento1,
+	SalaIntermediaEvento2,
+	SalaIntermediaEvento3,
+	Pista1,
+	Pista2,
+	Pista3,
+	Puzzle1,
+	Puzzle2,
+	Puzzle3
+};
 using RoomDialogues = std::unordered_map<std::string,std::list<TextInfo>>; //manage the events and the dialogues
 using RoomsMap = std::unordered_map<std::string, RoomDialogues>; //manage the rooms and there dialogues
 //A manager which function is to manage all the dialogues of the specific room
@@ -15,10 +26,14 @@ class DialogueManager
 {
 private:
 	const int numRooms = 3;
+	std::string room;
+	int actualroom;
 	RoomsMap mRoom; //Map with all the RoomDialogues
 	void ReadJson();
+	void ParseEnum(std::string& event, const eventToRead& _eventToRead);
 public:
 	DialogueManager();
-	void ReadDialogue(); 
+	void ReadDialogue(const eventToRead& _eventToRead); 
+	void ReadAnswer();
 };
 
