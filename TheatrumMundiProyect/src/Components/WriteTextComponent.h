bd61ec738cce467832ba5c85ed74e3274a825c82
Andrea G.A. <149385951+andreaGonzAnas@users.noreply.github.com>
2025-02-19 12:13:12 +0100

    Mas comentarios

--- TheatrumMundiProyect/src/Components/WriteTextComponent.h
+++ TheatrumMundiProyect/src/Components/WriteTextComponent.h
@@ -15,6 +15,8 @@ private:
 	Font& _myFont;
 	SDL_Color _color; // White
 	T* textStructure; //points to text structure. Log list or TextInfo.
+	// If it is log list, there is no Dynamic memory to destroy
+	// If it is TextInfo, DialogueManager already destroys that memory.
 public:
 	//ID
 	__CMPID_DECL__(ecs::cmp::WRITE_TEXT_COMPONENT);
