22e08d3c4a366ed4477adaac20ea3c03d0df3067
Claudia <149375895+Clauudz@users.noreply.github.com>
2025-02-20 20:08:41 +0100

    Eliminacion de clases innecesarias (mias)

--- TheatrumMundiProyect/TheatrumMundi/Gloves.cpp
+++
@@ -1,5 +0,0 @@
-/*#include "Gloves.h"
-
-Gloves::Gloves(Image* img) : HintTemplate("Gloves", img)
-{
-}*/
--- TheatrumMundiProyect/TheatrumMundi/Gloves.h
+++
@@ -1,9 +0,0 @@
-/*#pragma once
-#include "HintTemplate.h"
-
-
-class Gloves : public HintTemplate {
-public:
-	Gloves(Image* img);
-	
-};*/
