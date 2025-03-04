a3f7f4adc34253b62e1b971f74472f1667b2f487
Andrea G.A. <149385951+andreaGonzAnas@users.noreply.github.com>
2025-03-04 11:16:53 +0100

    menu inicial seteado

--- TheatrumMundiProyect/TheatrumMundi/InitialScene.cpp
+++ TheatrumMundiProyect/TheatrumMundi/InitialScene.cpp
@@ -21,10 +21,20 @@ InitialScene::~InitialScene()
 void InitialScene::init()
 {
 	if (!isStarted) {
-		//Start room1
+		//Background
+		auto _background = entityManager->addEntity();
+		entityManager->addComponent<Transform>(_background, Vector2D(0, 0), Vector2D(0, 0),2019/1.5, 1122/1.5, 0);
+		entityManager->addComponent<Image>(_background, &sdlutils().images().at("Room"));
 		
+		
+		//Title
+		auto _title = entityManager->addEntity();
+		entityManager->addComponent<Transform>(_title, Vector2D(sdlutils().width() / 4.5, sdlutils().height() /4), Vector2D(0, 0), 1470 / 2, 270 / 2, 0);
+		entityManager->addComponent<Image>(_title, &sdlutils().images().at("Title"));
+		
+		//Start button room1
 		auto _startbtn = entityManager->addEntity();
-		entityManager->addComponent<Transform>(_startbtn, Vector2D(sdlutils().width()/2.8, sdlutils().height()/2.5), Vector2D(0, 0), 1470/4, 270/4, 0);
+		entityManager->addComponent<Transform>(_startbtn, Vector2D(sdlutils().width()/2.8, sdlutils().height()/2), Vector2D(0, 0), 1470/4, 270/4, 0);
 		entityManager->addComponent<Image>(_startbtn, &sdlutils().images().at("NewGame"));
 
 		entityManager->addComponent<RectArea2D>(_startbtn);
