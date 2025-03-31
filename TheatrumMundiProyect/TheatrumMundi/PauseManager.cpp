d9ce59c10d3ea829b9d118ca200079f95a0e3fb9
Iker-kun <ikerjuma@ucm.es>
2025-03-31 16:45:59 +0200

    Colocacion de cosas

--- TheatrumMundiProyect/TheatrumMundi/PauseManager.cpp
+++ TheatrumMundiProyect/TheatrumMundi/PauseManager.cpp
@@ -42,7 +42,7 @@ void PauseManager::Init(EntityFactory* entityFactory, ecs::EntityManager* entity
 	entityManager->setActive(_backgroundPause, false);
 
 	//GO TO ROOM
-	auto reanudePauseButton = entityFactory->CreateInteractableEntity(entityManager, "B1", EntityFactory::RECTAREA, Vector2D(0, 0), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::INTERACTPAUSE);
+	auto reanudePauseButton = entityFactory->CreateInteractableEntity(entityManager, "B3", EntityFactory::RECTAREA, Vector2D(470, 270), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::INTERACTPAUSE);
 	entityManager->setActive(reanudePauseButton, false);
 	ClickComponent* buttonReanudePauseClick = entityManager->getComponent<ClickComponent>(reanudePauseButton);
 	buttonReanudePauseClick->connect(ClickComponent::JUST_CLICKED, [this, buttonSound, entityManager]() {
@@ -52,7 +52,7 @@ void PauseManager::Init(EntityFactory* entityFactory, ecs::EntityManager* entity
 		});
 
 	//GO TO THE INITIALMENU SCENE
-	auto exitPauseButton = entityFactory->CreateInteractableEntity(entityManager, "B1", EntityFactory::RECTAREA, Vector2D(0, 200), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::INTERACTPAUSE);
+	auto exitPauseButton = entityFactory->CreateInteractableEntity(entityManager, "B1", EntityFactory::RECTAREA, Vector2D(470, 370), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::INTERACTPAUSE);
 	entityManager->setActive(exitPauseButton, false);
 	ClickComponent* buttonExitPauseClick = entityManager->getComponent<ClickComponent>(exitPauseButton);
 	buttonExitPauseClick->connect(ClickComponent::JUST_CLICKED, [this, buttonSound, entityManager]() {
