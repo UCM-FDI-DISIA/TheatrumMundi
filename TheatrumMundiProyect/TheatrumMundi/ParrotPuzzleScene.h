e7e25a6bda6501a89d9e553820fa30dffdcf820d
TelmoK <telmo.kiss@gmail.com>
2025-05-12 10:05:35 +0200

    Parrot Scene setting

--- TheatrumMundiProyect/TheatrumMundi/ParrotPuzzleScene.h
+++ TheatrumMundiProyect/TheatrumMundi/ParrotPuzzleScene.h
@@ -1,13 +1,32 @@
 #pragma once
 #include "ScenePuzzleTemplate.h"
+#include <SDL.h>
 
 class ParrotPuzzleScene : public ScenePuzzleTemplate
 {
+public:
 	ParrotPuzzleScene();
 	~ParrotPuzzleScene();
 
 	void init(SceneRoomTemplate* sr) override;
 	void unload() override;
 	bool Check() override;
+
+private:
+
+	void _setGlobalFeatures();
+
+	void _setRoomBackground();
+	void _setInteractuables();
+	void _setDialog();
+	void _setUI();
+
+	struct ParrotUtils {
+		Uint32 lastSoundTime = 0; // The time where the parrot emited the last noise
+		std::vector<std::shared_ptr<Sound>> codeSequenceSounds; // Sequence of sounds in the second phase
+		int codeSeqIteration = 0;
+	} parrotUtils;
+
+	enum ParrotState { SHOOTING_SOUND, RED_LIGHTS };
 };
 
