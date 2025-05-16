830ab130c054fcc1751db43144f427f6fb3605c4
Ikerjum <ikerjuma@ucm.es>
2025-05-16 12:55:38 +0200

    Inventarios bien puestos en todos los puzzles

--- TheatrumMundiProyect/TheatrumMundi/Box.cpp
+++ TheatrumMundiProyect/TheatrumMundi/Box.cpp
@@ -34,9 +34,14 @@ void Box::init(SceneRoomTemplate* sr)
 			isStarted = true;
 			//Register scene in dialogue manager
 			dialogueManager->setScene(this);
+
+			AudioManager& a = AudioManager::Instance();
+			std::shared_ptr<Sound> buttonSound = sdlutils().soundEffects().at("boton");
+			a.setVolume(buttonSound, 0.2);
 			
 			room = sr;
 			//create the buttons
+
 			
 			//3,5,8,14
 
@@ -174,80 +179,18 @@ void Box::init(SceneRoomTemplate* sr)
 
 #pragma region Inventory
 
-			//INVENTORY
-			//Invntory Background
-			auto InventoryBackground = entityFactory->CreateImageEntity(entityManager, "fondoPruebaLog", Vector2D(1150, 0), Vector2D(0, 0), 300, 1500, 0, ecs::grp::DEFAULT);
-			entityManager->setActive(InventoryBackground, false);
-
-			auto upButton = entityFactory->CreateInteractableEntity(entityManager, "B6", EntityFactory::RECTAREA, Vector2D(1170, 70), Vector2D(0, 0), 70, 70, -90, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
-			entityManager->setActive(upButton, false);
-
-			auto downButton = entityFactory->CreateInteractableEntity(entityManager, "B6", EntityFactory::RECTAREA, Vector2D(1170, 748 - 268 / 3 - 20), Vector2D(0, 0), 70, 70, 90, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
-			entityManager->setActive(downButton, false);
-
-			
-			//InventoryButton
-			
-			auto inventoryButton = entityFactory->CreateInteractableEntity(entityManager, "B2", EntityFactory::RECTAREA, Vector2D(60 + 268 / 3, 20), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
-			ClickComponent* invOpen = entityManager->addComponent<ClickComponent>(inventoryButton);
-			invOpen->connect(ClickComponent::JUST_CLICKED, [this, sr, InventoryBackground, upButton, downButton, inventoryButton]() //Lamda function
-				{
-					//AudioManager::Instance().playSound(buttonSound);
-					sr->GetInventory()->setActive(!sr->GetInventory()->getActive());  // Toggle the inventory
-
-					// If the inventory is active, activate the items
-					if (sr->GetInventory()->getActive()) {
-						entityManager->setActive(InventoryBackground, true);
-
-						inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(925);
-						entityManager->setActive(downButton, true);
-						entityManager->setActive(upButton, true);
-
-						for (int i = 0; i < sr->GetInventory()->getItemNumber(); ++i) {
-							invObjects[i]->getMngr()->setActive(invObjects[i], true);
-						}
-					}
-					else {
-						entityManager->setActive(InventoryBackground, false);
-						entityManager->setActive(InventoryBackground, false);
-						entityManager->setActive(downButton, false);
-						entityManager->setActive(upButton, false);
-						inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(60 + 268 / 3);
-
-						for (int i = 0; i < sr->GetInventory()->getItemNumber(); ++i) {
-							invObjects[i]->getMngr()->setActive(invObjects[i], false);
-						}
-					}
-				});
-
-			ClickComponent* UPbuttonInventoryClick = entityManager->getComponent<ClickComponent>(upButton);
-			UPbuttonInventoryClick->connect(ClickComponent::JUST_CLICKED, [this, upButton, sr]() {
-
-				//AudioManager::Instance().playSound(buttonSound);
-				sr->scrollInventory(-1);
-				});
-
-			ClickComponent* DOWNbuttonInventoryClick = entityManager->getComponent<ClickComponent>(downButton);
-			DOWNbuttonInventoryClick->connect(ClickComponent::JUST_CLICKED, [this, downButton, sr]() {
-
-				//AudioManager::Instance().playSound(buttonSound);
-				sr->scrollInventory(1);
-				});
-
-			
-			
-			//Log
-
-
-
+		//INVENTORY
+		createInventoryUI();
 
 		//BackButton
 			auto _backButton = entityFactory->CreateInteractableEntity(entityManager, "B1", EntityFactory::RECTAREA, Vector2D(20, 20), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
 
 			//Click component Open log button
 			ClickComponent* clkOpen = entityManager->addComponent<ClickComponent>(_backButton);
-			clkOpen->connect(ClickComponent::JUST_CLICKED, [this, inventoryButton, InventoryBackground, downButton, upButton, _backButton]()
+			clkOpen->connect(ClickComponent::JUST_CLICKED, [this, _backButton, buttonSound]()
 				{
+					AudioManager::Instance().playSound(buttonSound);
+
 					inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(60 + 268 / 3);
 					HideInventoryItems();
 					room->GetInventory()->setFirstItem(0);
@@ -260,13 +203,15 @@ void Box::init(SceneRoomTemplate* sr)
 
 			//Log
 			dialogueManager->Init(0, entityFactory, entityManager, true, areaLayerManager, "SalaIntermedia1");
-			Game::Instance()->getLog()->Init(entityFactory, entityManager, areaLayerManager, this);
+			logbtn = Game::Instance()->getLog()->Init(entityFactory, entityManager, areaLayerManager, this);
+			//Game::Instance()->getLog()->Init(entityFactory, entityManager, areaLayerManager, this);
 			//startDialogue("Puerta");
 
 #pragma endregion
 
 		}
-		//createInvEntities(sr);
+		sr->GetInventory()->setFirstItem(0);
+		createInvEntities(sr);
 	
 }
 
--- TheatrumMundiProyect/TheatrumMundi/LockerPuzzle.cpp
+++ TheatrumMundiProyect/TheatrumMundi/LockerPuzzle.cpp
@@ -90,83 +90,27 @@ void LockerPuzzle::init(SceneRoomTemplate* sr)
             });
         doorEntities.push_back(checkbtn);
 
-        //INVENTORY
-       //Invntory Background
-        auto InventoryBackground = entityFactory->CreateImageEntity(entityManager, "fondoPruebaLog", Vector2D(1050, 0), Vector2D(0, 0), 300, 1500, 0, ecs::grp::DEFAULT);
-        entityManager->setActive(InventoryBackground, false);
+       //INVENTORY
+       createInventoryUI();
 
-        auto upButton = entityFactory->CreateInteractableEntity(entityManager, "B6", EntityFactory::RECTAREA, Vector2D(1170, 70), Vector2D(0, 0), 70, 70, -90, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
-        entityManager->setActive(upButton, false);
+       //BackButton
+       auto _backButton = entityFactory->CreateInteractableEntity(entityManager, "B1", EntityFactory::RECTAREA, Vector2D(20, 20), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::BOOKS_PUZZLE_SCENE_INTERACTABLE_INITIAL);
 
-        auto downButton = entityFactory->CreateInteractableEntity(entityManager, "B6", EntityFactory::RECTAREA, Vector2D(1170, 748 - 268 / 3 - 20), Vector2D(0, 0), 70, 70, 90, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
-        entityManager->setActive(downButton, false);
 
-        //InventoryButton
-        auto inventoryButton = entityFactory->CreateInteractableEntity(entityManager, "B2", EntityFactory::RECTAREA, Vector2D(40 + 268 / 3, 20), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::UI);
-        ClickComponent* invOpen = entityManager->addComponent<ClickComponent>(inventoryButton);
-        invOpen->connect(ClickComponent::JUST_CLICKED, [this, sr, InventoryBackground, upButton, downButton, inventoryButton, buttonSound]() //Lamda function
-            {
-                AudioManager::Instance().playSound(buttonSound);
-                sr->GetInventory()->setActive(!sr->GetInventory()->getActive());  // Toggle the inventory
-
-                // If the inventory is active, activate the items
-                if (sr->GetInventory()->getActive()) {
-                    entityManager->setActive(InventoryBackground, true);
-                    entityManager->setActive(logbtn, false);
-
-                    inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(925);
-                    entityManager->setActive(downButton, true);
-                    entityManager->setActive(upButton, true);
-
-                    for (int i = sr->GetInventory()->getFirstItem(); i < sr->GetInventory()->getFirstItem() + sr->GetInventory()->getItemNumber(); ++i) {
-                        invObjects[i]->getMngr()->setActive(invObjects[i], true);
-                    }
-
-                }
-                else {
-                    entityManager->setActive(InventoryBackground, false);
-                    entityManager->setActive(logbtn, true);
-                    entityManager->setActive(downButton, false);
-                    entityManager->setActive(upButton, false);
-                    inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(60 + 268 / 3);
-
-                    for (int i = sr->GetInventory()->getFirstItem(); i < sr->GetInventory()->getFirstItem() + sr->GetInventory()->getItemNumber(); ++i) {
-                        invObjects[i]->getMngr()->setActive(invObjects[i], false);
-                    }
-                }
-            });
-
-        ClickComponent* UPbuttonInventoryClick = entityManager->getComponent<ClickComponent>(upButton);
-        UPbuttonInventoryClick->connect(ClickComponent::JUST_CLICKED, [this, buttonSound, upButton, sr]() {
-
-            AudioManager::Instance().playSound(buttonSound);
-            scrollInventoryPuzzle(-1, sr);
-            });
-
-        ClickComponent* DOWNbuttonInventoryClick = entityManager->getComponent<ClickComponent>(downButton);
-        DOWNbuttonInventoryClick->connect(ClickComponent::JUST_CLICKED, [this, buttonSound, downButton, sr]() {
-
-            AudioManager::Instance().playSound(buttonSound);
-            scrollInventoryPuzzle(1, sr);
-            });
-
-        //BackButton
-        auto _backButton = entityFactory->CreateInteractableEntity(entityManager, "B1", EntityFactory::RECTAREA, Vector2D(20, 20), Vector2D(0, 0), 90, 90, 0, areaLayerManager, EntityFactory::NODRAG, ecs::grp::BOOKS_PUZZLE_SCENE_INTERACTABLE_INITIAL);
-
-        //Click component Open log button
-        ClickComponent* clkOpen = entityManager->addComponent<ClickComponent>(_backButton);
-        clkOpen->connect(ClickComponent::JUST_CLICKED, [this, sr, InventoryBackground, downButton, upButton, inventoryButton, _backButton, buttonSound]()
-            {
-                AudioManager::Instance().playSound(buttonSound);
-                inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(60 + 268 / 3);
-                HideInventoryItems();
-                sr->GetInventory()->setFirstItem(0);
-                auto _backButtonImage = _backButton->getMngr()->getComponent<Image>(_backButton);
-                _backButtonImage->setW(_backButton->getMngr()->getComponent<Transform>(_backButton)->getWidth());
-                _backButtonImage->setH(_backButton->getMngr()->getComponent<Transform>(_backButton)->getHeight());
-                _backButtonImage->setPosOffset(0, 0);
-                Game::Instance()->getSceneManager()->popScene();
-            });
+       //Click component Open log button
+       ClickComponent* clkOpen = entityManager->addComponent<ClickComponent>(_backButton);
+       clkOpen->connect(ClickComponent::JUST_CLICKED, [this, _backButton, buttonSound]()
+           {
+               AudioManager::Instance().playSound(buttonSound);
+               inventoryButton->getMngr()->getComponent<Transform>(inventoryButton)->setPosX(60 + 268 / 3);
+               HideInventoryItems();
+               room->GetInventory()->setFirstItem(0);
+               auto _backButtonImage = _backButton->getMngr()->getComponent<Image>(_backButton);
+               _backButtonImage->setW(_backButton->getMngr()->getComponent<Transform>(_backButton)->getWidth());
+               _backButtonImage->setH(_backButton->getMngr()->getComponent<Transform>(_backButton)->getHeight());
+               _backButtonImage->setPosOffset(0, 0);
+               Game::Instance()->getSceneManager()->popScene();
+           }); 
 
         dialogueManager->Init(1, entityFactory, entityManager, false, areaLayerManager, "SalaIntermedia1");
         logbtn = Game::Instance()->getLog()->Init(entityFactory, entityManager, areaLayerManager, this);
