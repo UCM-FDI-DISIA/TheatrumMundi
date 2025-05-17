ad5794a9c658848878857749548bb176e41b9e08
Reycka <157039413+Reycka@users.noreply.github.com>
2025-05-17 21:07:16 +0200

    Mosaico 4x4

--- TheatrumMundiProyect/TheatrumMundi/MosaicPuzzleScene.h
+++ TheatrumMundiProyect/TheatrumMundi/MosaicPuzzleScene.h
@@ -7,11 +7,11 @@ class MosaicPuzzleScene : public ScenePuzzleTemplate
 private:
 
 	bool squareMoving = false; //if one mosaic piece is moving or not (evitates to move one while other is moving)
-	const int TOTALSQUARES = 8; //Total of interactuable squares of the mosaic
-	const int SQUAREWIDTH = 200; // Size of each square
-	std::vector<std::string>imgId = {"B1","B2","B3","B4","B5","B6","B7","B1"}; //array of the images of each square
+	const int TOTALSQUARES = 15; //Total of interactuable squares of the mosaic
+	const int SQUAREWIDTH = 150; // Size of each square
+	std::vector<std::string>imgId = {"B1","B2","B3","B4","B5","B6","B7","B1","B1","B1","B1","B1","B1","B1","B1"}; //array of the images of each square
 	std::vector<Vector2D> positions; //Positions of the 
-	std::vector<int> indexPositions = {5,3,6,0,2,1,4,7}; //Sets the positions of 
+	std::vector<int> indexPositions = {2,7,4,6,0,1,13,10,8,12,5,11,3,14,9}; //Sets the positions of 
 	Transform* square = nullptr; //Square clicked
 	Vector2D freePos; //Saves the free spot Position
 	Vector2D originalPos; //Saves the original Pos of the square which is gonna move
